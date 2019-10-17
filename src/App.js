31afaef64fe672851c9ee0f829619ba5e8655ed3
Amanda Giannelli <agiannelli23@gmail.com>
2019-10-17 09:09:05 -0400

    Modularize code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './lib/Game'
+
+export default function App() {
+  //TODO: Allow player vs. computer
+  // ========================================
+  return (
+    <>
+      <Game />
+    </>
+  );
+
+}
